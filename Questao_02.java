67793eb150829a08e21f81cf85eebee4fb535e8f
Wellington Miguel <95249930+Wellington-Miguel@users.noreply.github.com>
2022-02-17 09:38:22 -0300

    Update Questão 02 - Comments

--- Questao_02.java
+++ Questao_02.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
    String senha ;
+    // input senha
     try (Scanner input = new Scanner(System.in)) {
         System.out.print("Informe a senha: ");
         senha = input.nextLine();
       }
+    //Verificando tamanho da senha
       if (senha.length()<6){
         System.out.printf("Faltam %d caractere(s) \n" ,6 - senha.length());
       }else{
